0d1656e898a1884648eb176d27388cd0cbf8cc72
lixy16 <lixy16@ziroom.com>
2018-08-15 08:41:51 +0800

    增加仪表盘

--- src/main/java/com/bbrightway/servicefeign/ServiceFeignApplication.java
+++ src/main/java/com/bbrightway/servicefeign/ServiceFeignApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableHystrixDashboard
 public class ServiceFeignApplication {
 
 	public static void main(String[] args) {
